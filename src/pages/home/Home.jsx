13e10f0a27d32a7696141b21e8ada5818004179d
Abhishek Vats <abhishekvats010.av@gmail.com>
2022-04-09 15:19:35 +0530

    First commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import SideBar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
+import Widgets from '../../components/widgets/Widgets';
+import Featured from '../../components/featured/Featured';
+import Chart from '../../components/chart/Chart';
+import Table from '../../components/table/Table';
+
+import './home.scss'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <SideBar />
+        <div className="homeContainer">
+          <Navbar />
+          <div className="widgets">
+            <Widgets type='user' />
+            <Widgets type='order' />
+            <Widgets type='earnings' />
+            <Widgets type='balance' />
+          </div>
+          <div className="charts">
+            <Featured />
+            <Chart title="Last 6 months(Revenue)" aspect={2 / 1} />
+          </div>
+          <div className="listContainer">
+            <div className="listTitle">Latest Transactions</div>
+            <Table />
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
